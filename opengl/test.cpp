8c0bcf091befb0fe552ff2c1204dc8be702250ff
Vicent Chen <1336754721@qq.com>
2017-12-28 02:06:16 +0800

    Review window

--- opengl/test.cpp
+++ opengl/test.cpp
@@ -4,28 +4,27 @@
 
 using namespace std;
 
-const unsigned int SCR_WIDTH = 800;
-const unsigned int SCR_HEIGHT = 600;
-
-void process_input(GLFWwindow*);
-void framebuffer_size_callback(GLFWwindow*, int, int);
-
-const char* vertexShaderSource = "#version 330 core\n"
+const char* vertexShader = "#version 330 core\n"
 "layout (location = 0) in vec3 aPos;\n"
-"void main(){\n"
-"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"void main() {\n"
+"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0)\n"
+"}\0";
+const char* fragementShader = "#version 330 core\n"
+"out vec4 FragColor;"
+"void main() {\n"
+"   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
 "}\0";
 
-const char* fragementShaderSource = "#version 330 core\n"
-"out vec4 FragColor;\n"
-"void main() {\n"
-"   FragColor = vec4(0.9f, 0.2f, 0.8f, 1.0f);\n"
-"}\0";
+const int SCR_WIDTH = 800;
+const int SCR_HEIGHT = 600;
+
+void process_input(GLFWwindow *);
+void framebuffer_size_callback(GLFWwindow *, int, int);
 
 int main(int argc, char* argv[]) {
     glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     GLFWwindow *window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Test", NULL, NULL);
@@ -34,96 +33,25 @@ int main(int argc, char* argv[]) {
 
     gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
 
-    int success;
-    char infolog[512];
-    int vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-    glCompileShader(vertexShader);
-    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-    if (!success) {
-        glGetShaderInfoLog(vertexShader, 512, NULL, infolog);
-        cout << "vertex shader compile error:\n" << infolog << endl;
-    }
-
-    int fragementShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragementShader, 1, &fragementShaderSource, NULL);
-    glCompileShader(fragementShader);
-    glGetShaderiv(fragementShader, NULL, &success);
-    if (!success) {
-        glGetShaderInfoLog(fragementShader, 512, NULL, infolog);
-        cout << "fragement shader compile error:\n" << infolog << endl;
-    }
-
-    int shaderProgram = glCreateProgram();
-    glAttachShader(shaderProgram, vertexShader);
-    glAttachShader(shaderProgram, fragementShader);
-    glLinkProgram(shaderProgram);
-    glGetShaderiv(shaderProgram, NULL, &success);
-    if (!success) {
-        glGetShaderInfoLog(shaderProgram, 512, NULL, infolog);
-        cout << "shader program compile error:\n" << infolog << endl;
-    }
-    glDeleteShader(vertexShader);
-    glDeleteShader(fragementShader);
-
-    float vertices[] = {
-        0.5f,  0.5f, 0.0f,  // top right
-        0.5f, -0.5f, 0.0f,  // bottom right
-        -0.5f, -0.5f, 0.0f,  // bottom left
-        -0.5f,  0.5f, 0.0f   // top left 
-    };
-    unsigned int indices[] = {  // note that we start from 0!
-        0, 1, 3,   // first triangle
-        1, 2, 3    // second triangle
-    };
-
-    unsigned int VAO, VBO, EBO;
-    glGenVertexArrays(1, &VAO);
-    glGenBuffers(1, &VBO);
-    glGenBuffers(1, &EBO);
-    glBindVertexArray(VAO);
-
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
-
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-    glBindVertexArray(0);
-
-
-    while (!glfwWindowShouldClose(window)) {
+    while(!glfwWindowShouldClose(window)) {
         process_input(window);
 
-        glClearColor(0.0f, 0.8f, 0.2f, 0.9f);
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
-
-        glUseProgram(shaderProgram);
-        glBindVertexArray(VAO);
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-        glBindVertexArray(0);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
-    
-    glDeleteVertexArrays(1, &VAO);
-    glDeleteBuffers(1, &VBO);
 
     glfwTerminate();
     return 0;
 }
 
 void process_input(GLFWwindow *window) {
-    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, GLFW_TRUE);
-    }
 }
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
     glViewport(0, 0, width, height);
 }
