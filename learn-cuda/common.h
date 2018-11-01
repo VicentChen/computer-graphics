e133ea1d24315adc3d4c1e64d689e1b8f56437b6
VicentChen <1336754721@qq.com>
2018-11-01 20:34:55 +0800

    common header

---
+++ learn-cuda/common.h
@@ -0,0 +1,22 @@
+#include <sys/time.h>
+
+#ifndef _COMMON_H
+#define _COMMON_H
+
+#define CHECK(call) do { \
+  const cudaError_t error = call; \
+  if (error != cudaSuccess) { \
+    fprintf(stderr, "Error: %s:%d, ", __FILE__, __LINE__); \
+    fprintf(stderr, "code: %d, reason: %s\n", error, cudaGetErrorString(error)); \
+    exit(1); \
+  } \
+}while(0)
+
+inline double seconds() {
+  struct timeval tp;
+  struct timezone tzp;
+  int i = gettimeofday(&tp, &tzp);
+  return ((double)tp.tv_sec + (double)tp.tv_usec * 1.e-6);
+}
+
+#endif // _COMMON_H
